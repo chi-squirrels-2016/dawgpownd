f9c824f605ea44ccf988954619ebc94a6f7e47b2
Dan Turcza <datu925@gmail.com>
2016-02-17 14:56:13 -0600

    add login and register routes and views

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,1 +1,38 @@
+get '/sessions/login/?' do
+  erb :'sessions/login'
+end
 
+get '/sessions/register/?' do
+  erb :'sessions/register'
+end
+
+get '/sessions/logout/?' do
+  session[:user_id] = nil
+  erb :'sessions/logout'
+end
+
+post '/sessions/login/?' do
+  user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/questions"
+  else
+    @errors = ["Invalid login. Try again"]
+    erb :'sessions/login'
+  end
+end
+
+post '/sessions/register/?' do
+  user = User.new(first_name: params[:first_name],
+                  last_name: params[:last_name],
+                  email: params[:email]
+                  )
+  user.password = params[:password]
+  if user.save
+    session[:user_id] = user.id
+    redirect "/questions"
+  else
+    @errors = user.errors.full_messages
+    erb :'sessions/register'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,39 @@ class User < ActiveRecord::Base
   has_many :questions
 
 
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validate :password_length
+
+  def password
+      @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    if @user
+      if @user.password == password
+        @user
+      end
+    else
+      nil
+    end
+  end
+
+  def password_length
+    if @raw_password.nil?
+      errors.add(:base, "Password must be longer than 3 characters")
+    elsif @raw_password.length <= 3
+      errors.add(:base, "Password must be longer than 3 characters")
+      puts self.errors.full_messages
+    else
+    end
+  end
+
 end
