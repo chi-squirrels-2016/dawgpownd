411a3dab670f0024f837ca8c68af7ddbb0b168c5
ray-curran <rpcurr@gmail.com>
2016-02-17 16:08:12 -0600

    Get question and answer comments working, still broken in my chrome :(

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -18,3 +18,25 @@ post '/questions/:question_id/comments/new' do
     erb :'comments/new'
   end
 end
+
+get '/answers/:answer_id/comments/new' do
+  @answer = Answer.find(params[:answer_id]);
+  erb :'comments/new'
+end
+
+post '/answers/:answer_id/comments/new' do
+  @answer = Answer.find(params[:answer_id])
+  puts current_user
+  if current_user
+    @comment = @answer.comments.create(user_id: current_user.id, body: params[:body], commentable_type: "Answer")
+    if @comment.save
+      redirect "/answer/#{@answer.id}"
+    else
+      @errors = @comment.errors.full_messages
+      erb :'comments/new'
+    end
+  else
+    @errors = ['You must be logged in to post a comment!!']
+    erb :'comments/new'
+  end
+end
