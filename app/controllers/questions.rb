2cbdc5a02345588855e30f456b4835ab235fc80f
ray-curran <rpcurr@gmail.com>
2016-02-17 12:08:47 -0600

    Make post form actually create question objects linked to the current user

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -5,3 +5,14 @@ end
 get '/questions/new' do
 	erb :'questions/new'
 end
+
+post '/questions' do
+	session[:user_id] = 2
+	@question = current_user.questions.create(params[:question]);
+	p @question
+	if @question.save
+		redirect "/questions/#{@question.id}"
+	else
+		redirect '/questions/new'
+	end
+end
