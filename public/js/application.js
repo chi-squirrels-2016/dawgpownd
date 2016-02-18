f39cf69283dbcf36b0e5de4647ebf97b67252269
Dan Turcza <email": null>
2016-02-18 14:54:37 -0600

    Add post and comment form AJAX

--- public/js/application.js
+++ public/js/application.js
@@ -3,18 +3,20 @@ $(document).ready(function(){
   // GET answer form
   $('.post_answer').on('click', 'a', function(e){
     e.preventDefault();
+    var link = $(this);
     var request = $.ajax({
       type: "GET",
       url: $(this).attr('href')
     });
     request.done(function(data) {
       // append to whatever class the answers are in
-      $('footer').before(data);
+      $('#answers_container').after(data);
+      link.hide();
     });
   });
 
   // POST answer
-  $('#new-answer').on('submit', function(e) {
+  $('article').on('submit', "#new-answer", function(e) {
     e.preventDefault();
     var request = $.ajax({
       type: "POST",
@@ -23,14 +25,46 @@ $(document).ready(function(){
     });
     request.done(function(data) {
       // append answer to wherever
-      var xyz = data;
+      $('#answers_container').append(data);
+      $("#question-form").hide();
+      $(".post_answer").find("a").show();
     });
   });
 
   // comment form
+  $('article').on('click', ".comment-form-link", function(e){
+    e.preventDefault();
+    var link = $(this);
+    var comment_div = $(this).closest('.comments-div')
+    var request = $.ajax({
+      type: "GET",
+      url: $(this).attr('href')
+    });
+    request.done(function(data) {
+      // append to whatever class the answers are in
+      comment_div.find('ul').after(data);
+      link.hide();
+    });
+  });
 
 
 
+  $('article').on('submit', "#new-comment", function(e) {
+    e.preventDefault();
+    var commentList = $(this).closest('.comments-div');
+    var request = $.ajax({
+      type: "POST",
+      url: $(this).attr('action'),
+      data: $(this).serialize()
+    });
+    request.done(function(data) {
+      // append answer to wherever
+      commentList.find('ul').append(data);
+      console.log(commentList);
+      $("#question-form").hide();
+      commentList.find(".comment-form-link").show();
+    });
+  });
 
 
   // vote arrows
